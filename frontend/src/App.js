a3de0efe30fc924f546c8c436d0be8a23e9b8b05
AryanShelke <aryan.shelke.2003@gmail.com>
2024-06-04 13:10:25 -0700

    Add link saving on frontend
    - Add barebones UI for frontend
    - Use React hook to save YT video link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [videoLink, setVideoLink] = useState("");
+  const [savedLink, setSavedLink] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSavedLink(videoLink);
+    setVideoLink("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>YT Rehashed</h1>
       </header>
+      <p>Enter the link to summarize the YT video:</p><br></br>
+      <input 
+        type="text"
+        value={videoLink}
+        placeholder="https://www.youtube.com/watch?v="
+        onChange={(e) => setVideoLink(e.target.value)}>
+      </input>
+      <button onClick={handleSubmit}>Summarize</button>
+      {savedLink && (
+        <div>
+          <h2>Saved Link:</h2>
+          <p>{savedLink}</p>
+        </div>
+      )}
     </div>
   );
 }
