6a57d24fa8614e56d86c9b4736bb11a45e85e5fb
AryanShelke <aryan.shelke.2003@gmail.com>
2024-06-04 21:37:58 -0700

    Add transcript retrieval
    - Use Youtube Transcript API to grab transcripts for videos
    - Build backend API to handle url sent from frontend
    - Display full transcript with text put into one string on the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,39 @@
 const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const transcriptAPI = require('youtube-transcript-api');
 
 const app = express();
 const PORT = 8080;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+function extractVideoId(url) {
+  const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S*\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
+}
+
+// API endpoint to handle transcript requests
+app.post('/api/get-transcript', async (req, res) => {
+  const videoID = extractVideoId(req.body.videoUrl);
+
+  if (videoID === null) {
+    res.status(400).json({ message: "Invalid URL" });
+  }
+
+  transcriptAPI.getTranscript(videoID)
+    .then((transcriptList) => {
+      let transcriptStr = "";
+      transcriptList.forEach((caption) => {
+        transcriptStr += ' ' + caption.text;
+      });
+      res.status(200).json({ transcript: transcriptStr });
+    })
+    .catch(error => {
+      res.status(500).json({ message : error.message } );
+      console.log();
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [videoLink, setVideoLink] = useState("");
-  const [savedLink, setSavedLink] = useState("");
+  const [videoTranscript, setTranscript] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setSavedLink(videoLink);
+    let url = videoLink;
     setVideoLink("");
+
+    axios.post('/api/get-transcript', { videoUrl: url })
+      .then(response => {
+        setTranscript(response.data.transcript);
+      })
+      .catch(error => {
+        console.error(error);
+        setTranscript("Invalid URL!");
+    });
+    
   };
 
   return (
@@ -17,17 +28,20 @@ function App() {
         <h1>YT Rehashed</h1>
       </header>
       <p>Enter the link to summarize the YT video:</p><br></br>
-      <input 
-        type="text"
-        value={videoLink}
-        placeholder="https://www.youtube.com/watch?v="
-        onChange={(e) => setVideoLink(e.target.value)}>
-      </input>
-      <button onClick={handleSubmit}>Summarize</button>
-      {savedLink && (
+      <div>
+        <input 
+          type="text"
+          value={videoLink}
+          placeholder="https://www.youtube.com/watch?v="
+          onChange={(e) => setVideoLink(e.target.value)}>
+        </input>
+        <button onClick={handleSubmit}>Summarize</button>
+      </div>
+      
+      {videoTranscript && (
         <div>
-          <h2>Saved Link:</h2>
-          <p>{savedLink}</p>
+          <h2>Video Transcript:</h2>
+          <p>{videoTranscript}</p>
         </div>
       )}
     </div>
