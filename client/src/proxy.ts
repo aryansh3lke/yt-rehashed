ab7271efbb56c94f60f43a38fbb214f46c2e311d
AryanShelke <aryan.shelke.2003@gmail.com>
2025-03-09 15:02:32 -0700

    Update proxy.ts

--- client/src/proxy.ts
+++ client/src/proxy.ts
@@ -1,5 +1,6 @@
 // Access Vercel environment variable in production to reach deployed backend server on Railway
-// export const PROXY_URL =
-//   process.env.REACT_APP_PROXY_URL || "http://localhost:8000";
+export const PROXY_URL =
+  process.env.REACT_APP_PROXY_URL || "http://localhost:8000";
 
-export const PROXY_URL = "https://yt-rehashed-test.up.railway.app";
+// For other branches to test deployment on Railway
+// export const PROXY_URL = "https://yt-rehashed-test.up.railway.app";
